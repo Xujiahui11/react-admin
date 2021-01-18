2105f6ae69e368ac6954becdfe2edf948158ace3
xjh <838611341@qq.com>
2021-01-19 00:37:05 +0800

    ...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { HashRouter, NavLink, Switch, Route } from 'react-router-dom'
+import Home from './views/Home'
+import About from './views/About'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+  render () {
+    return (
+      <HashRouter>
+        <Switch>
+          <Route exact component={Home} path='/' />
+          <Route component={About} path='/about' />
+        </Switch>
+      </HashRouter>
+    )
+  }
 }
 
-export default App;
+export default App
